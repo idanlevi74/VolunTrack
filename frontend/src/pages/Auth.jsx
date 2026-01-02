546c663bb4578a1c107783cb6c2c17bf26a847b0
Idan Levi <idan@yadlabanim.org.il>
2026-01-02 17:46:35 +0200

    Fixing

--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "../styles/auth.css";
+
 import { apiFetch } from "../api/client";
 
 export default function Auth() {
